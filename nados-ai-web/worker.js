0372335e52150fca8de8058f5c4366c2af222d0a
Yousif Mohammed <DOD232211474@huc.edu.iq>
2026-09-22 20:23:50 +0300

    New tunnel for session

--- nados-ai-web/worker.js
+++ nados-ai-web/worker.js
@@ -1,4 +1,4 @@
-const API_TARGET = 'https://musicians-int-representatives-barbara.trycloudflare.com'
+const API_TARGET = 'https://shelf-collectors-industries-especially.trycloudflare.com'
 
 export default {
   async fetch(request, env) {
